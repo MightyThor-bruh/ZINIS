9abe0b0318534d321ba35923477e2097b32b80c2
maybelisa <73255984+MightyThor-bruh@users.noreply.github.com>
2022-12-31 21:00:37 +0300

    Add files via upload

---
+++ лаб10/LZ/LZ/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace LZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var originalMessage =
+                "213380732";
+            var N = 9; // Мощность алфавита
+            var n1 = 5;
+            var n2 = 5;
+            var ratio = 0;
+
+            Console.WriteLine($"Исходная строка:\n{originalMessage}");
+            {
+                Console.Write($"\nВведите n1 (по-умолчанию {n1}): ");
+                if (int.TryParse(Console.ReadLine(), out var newValue))
+                    n1 = newValue;
+                Console.Write($"Введите n2 (по-умолчанию {n2}): ");
+                if (int.TryParse(Console.ReadLine(), out newValue))
+                    n2 = newValue;
+            }
+            originalMessage = string.Join("", Enumerable.Repeat(' ', n1)) + originalMessage;
+
+            var triadLength = (int)(Math.Ceiling(Math.Log(n1, N)) + Math.Ceiling(Math.Log(n2, N)) + 1);
+            Console.WriteLine($"\nДлина триады: {triadLength}");
+
+            // Compression
+            Stopwatch ticksEncode = new Stopwatch();
+            ticksEncode.Start();
+            var compressedMessage = new StringBuilder();
+            {
+                var currentSymbol = n1;
+                while (currentSymbol < originalMessage.Length)
+                {
+                    var sequenceLength = 0;
+                    var offsetSequence = n1;
+                    int offsetSequenceLast;
+                    do
+                    {
+                        offsetSequenceLast = n1 - offsetSequence;
+                        sequenceLength++;
+                        var secondBufferLength = currentSymbol + n2 < originalMessage.Length
+                            ? n2 - 1
+                            : originalMessage.Length - currentSymbol;
+                        offsetSequence = originalMessage.Substring(currentSymbol - n1, n1 + secondBufferLength)
+                            .IndexOf(originalMessage.Substring(currentSymbol, sequenceLength),
+                                StringComparison.Ordinal);
+                        if (offsetSequence >= n1)
+                            offsetSequence = -1;
+                    } while (offsetSequence >= 0);
+
+                    sequenceLength--;
+                    currentSymbol += sequenceLength;
+                    compressedMessage.Append(string.Format(
+                        $"{(char)(97 + offsetSequenceLast)}{(char)(97 + sequenceLength)}{originalMessage.Substring(currentSymbol, 1)} "));
+                    currentSymbol += 1;
+                }
+
+                Console.WriteLine($"\nПолученная строка:\n{compressedMessage}");
+            }
+            ticksEncode.Stop();
+            // Decompression
+            Stopwatch ticksDecode = new Stopwatch();
+            ticksDecode.Start();
+            {
+                var currentSymbol = 0;
+                var message = compressedMessage.ToString();
+                var decompressedString = new StringBuilder();
+                while (compressedMessage.Length > currentSymbol)
+                {
+                    var triade = message.Substring(currentSymbol, triadLength + 1);
+                    var p = triade[0] - 97;
+                    var q = triade[1] - 97;
+                    if (p == 0)
+                        decompressedString.Append(triade[2]);
+                    else
+                    {
+                        while (q - p >= 0)
+                        {
+                            decompressedString.Append(decompressedString.ToString()[(decompressedString.Length - p)..]);
+                            q -= p;
+                        }
+
+                        decompressedString.Append(decompressedString.ToString()
+                            .Substring(decompressedString.Length - p, q));
+                        decompressedString.Append(triade[2]);
+                    }
+
+                    currentSymbol += triadLength + 1;
+                }
+                ticksDecode.Stop();
+                Console.WriteLine($"\n Распакованная строка:\n{decompressedString}");
+                ratio = decompressedString.Length / compressedMessage.Length;
+                Console.WriteLine($"\n Эффективность:\n{ratio}");
+                Console.WriteLine("Скорость кодирования: " + ticksEncode.Elapsed);
+                Console.WriteLine("Скорость декодирования: " + ticksDecode.Elapsed + "\n");
+            }
+        }
+    }
+}
---
+++ лаб11/ArithmeticMethod/ArithmeticMethod/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArithmeticMethod
+{
+    class Program
+    {
+        private struct Section
+        {
+            public double Left;
+            public double Right;
+        }
+        static void Main(string[] args)
+        {
+            var originMessage1 = "гидроаэроионизация";
+            var originMessage2 = "гидроаэроионизацияэкстраординарный";
+
+            DoArithmeticCodeAndDecode(originMessage1);
+            DoArithmeticCodeAndDecode(originMessage2);
+        }
+        private static void DoArithmeticCodeAndDecode(string message)
+        {
+            Console.WriteLine($"Исходное сообщение: {message}");
+
+            var probabilitySections = GetProbabilitySections(message);
+            Console.WriteLine("Полученные вероятности:");
+            foreach (var (letter, probability) in probabilitySections)
+                Console.Write($"{letter}-{probability.Right - probability.Left} ");
+            Console.WriteLine();
+
+            var code = ArithmeticCode(message, probabilitySections);
+            Console.WriteLine($"Полученное число: {code}");
+
+            var decodedMessage = ArithmeticDecode(probabilitySections, code, message.Length);
+            Console.WriteLine($"Расшифрованное сообщение: {decodedMessage}\n");
+        }
+
+        private static Dictionary<char, Section> GetProbabilitySections(string text)
+        {
+            Dictionary<char, double> probabilities = new();
+            foreach (var letter in text)
+                if (probabilities.ContainsKey(letter))
+                    probabilities[letter]++;
+                else
+                    probabilities.Add(letter, 1);
+
+            Dictionary<char, Section> probabilitySections = new();
+            double currentProbability = 0;
+            foreach (var keyPair in probabilities)
+            {
+                var probability = keyPair.Value / text.Length;
+                probabilitySections.Add(keyPair.Key,
+                    new Section { Left = currentProbability, Right = currentProbability + probability });
+                currentProbability += probability;
+            }
+
+            return probabilitySections;
+        }
+
+        private static double ArithmeticCode(string originMessage, Dictionary<char, Section> probabilitySetions)
+        {
+            var current = new Section { Left = 0, Right = 1 };
+            foreach (var letter in originMessage)
+            {
+                var newRight = current.Left + (current.Right - current.Left) * probabilitySetions[letter].Right;
+                current.Left = current.Left + (current.Right - current.Left) * probabilitySetions[letter].Left;
+                current.Right = newRight;
+            }
+
+            return (current.Left + current.Right) / 2;
+        }
+
+        private static string ArithmeticDecode(Dictionary<char, Section> probabilitySections, double code,
+            int messageLength)
+        {
+            StringBuilder decodedMessage = new();
+            var arrayLetters = probabilitySections.Keys.ToList();
+            for (var i = 0; i < messageLength; i++)
+                for (var j = 0; j < probabilitySections.Count; j++)
+                    if (code > probabilitySections[arrayLetters[j]].Left &&
+                        code < probabilitySections[arrayLetters[j]].Right)
+                    {
+                        decodedMessage.Append(arrayLetters[j]);
+                        code = (code - probabilitySections[arrayLetters[j]].Left) /
+                               (probabilitySections[arrayLetters[j]].Right - probabilitySections[arrayLetters[j]].Left);
+                        break;
+                    }
+
+            return decodedMessage.ToString();
+        }
+    }
+}
